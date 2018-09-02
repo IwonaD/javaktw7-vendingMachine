d659f7b2fccaff589b5217535e088eaac24d4cf0
IwonaD <Iwonadanel1>
2018-09-02 11:44:34 +0200

    added printing cell with symbols. Loops and methods for vending machine

---
+++ src/main/java/pl/sdacademy/vending/controller/CustomerOperationController.java
@@ -0,0 +1,73 @@
+package pl.sdacademy.vending.controller;
+
+import pl.sdacademy.vending.model.VendingMachine;
+
+public class CustomerOperationController {
+
+    //skomponowaLISMY OBIEKT DO INNEJ KLASY. To jest kompozycja. Lepsza niz dziedziczenie
+
+    private VendingMachine machine;
+
+    public CustomerOperationController() {
+        machine = new VendingMachine();
+    }
+
+    public void printmachine() {
+
+        for (int row = 0; row <= machine.rowsSize(); row++) {
+            for (int col = 0; col < machine.colSize(); col++) {
+                //wyswietl gorna belke dla komorki o koordynatach col row
+                printUpperBoundaryForCell(row, col);
+                //drukuj belke
+            }
+            System.out.println();
+            for (int col = 0; col < machine.colSize(); col++) {
+                //drukuj symbole
+                printSymbolForCell(row, col);
+            }
+            System.out.println();
+            for (int col = 0; col < machine.colSize(); col++) {
+                //drukuj belke
+                printLowerBoundaryForCell(row, col);
+            }
+            System.out.println();
+        }
+
+        /* dla kazdego wiersza wyswietlic:
+            dla kazdej z kolumn wyswietl:
+
+        -gorna czesc ( +----+)
+        -boki oraz numer
+        -dolna czesc (+-----+)
+         */
+
+
+//        for (int i = 0; i < machine.rowsSize(); i++) {
+//            for ( int j = 0; j < machine.colSize(); j++) {
+//                System.out.println("+------------+");
+//            }
+//            System.out.println();
+//
+//            for (int j = 0; j<machine.colSize(); j++ ) {
+//                System.out.println(" + |  +");
+//            }
+//        }
+    }
+    private void printUpperBoundaryForCell(int row, int col) {
+        System.out.print("+--------+");
+    }
+    private void printSymbolForCell(int row, int col) {
+        char rowSymbol = (char) ('A' + row);
+        int colSymbol = col + 1;
+        System.out.print("|   " + rowSymbol + colSymbol + "   |");
+    }
+
+    private void printLowerBoundaryForCell(int row, int col) {
+        System.out.print("+--------+");
+
+    }
+
+
+
+
+}
